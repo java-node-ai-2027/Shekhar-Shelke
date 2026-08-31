43e716d3f44c5a45d9b5fe4fc8933e1f0f9a4dea
shekhar-thinkitive <shekhar.shelke@thinkitive.com>
2026-08-31 20:19:39 +0530

    Day 12: fix: resolve code and lock file issues

--- Node/Day 12/server.js
+++ Node/Day 12/server.js
@@ -3,100 +3,100 @@
 import http from "http"
 // const math=require('./math')
 console.log("Hello world....!")
-// console.log(math.add(30,10))
-// console.log(math.sub(30,10))
-// const server =http.createServer((req,res)=>{
-//     // console.log(req.url)
-//     // console.log(req.headers)
-//     // res.statusCode=200;
-//     // res.end("Hello from node.js ")
+console.log(math.add(30,10))
+console.log(math.sub(30,10))
+const server =http.createServer((req,res)=>{
+    console.log(req.url)
+    console.log(req.headers)
+    res.statusCode=200;
+    res.end("Hello from node.js ")
 
-//     //  if (req.url === "/") {
-//     //     res.statusCode = 200;
-//     //     res.setHeader("Content-Type", "text/html");
+     if (req.url === "/") {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/html");
 
-//     // res.end(`
-//     //     <h1>Welcome</h1>
-//     //     <p>This page is coming from Node.js</p>
-//     // `);
+    res.end(`
+        <h1>Welcome</h1>
+        <p>This page is coming from Node.js</p>
+    `);
 
-//     // }
-//     // else {
-//     //     res.statusCode = 404;
-//     //     res.end("Page Not Found");
-//     // }
-//     // console.log(req.headers.host)
-//     // console.log(req.headers['user-agent'])
-//     // res.setHeader("Content-Type","application/json")
+    }
+    else {
+        res.statusCode = 404;
+        res.end("Page Not Found");
+    }
+    console.log(req.headers.host)
+    console.log(req.headers['user-agent'])
+    res.setHeader("Content-Type","application/json")
 
-//     // const student={
-//     //     name:"Shekhar",
-//     //     age:22,
-//     // }
+    const student={
+        name:"Shekhar",
+        age:22,
+    }
    
-//     // res.end(JSON.stringify(student))
-//     // res.end(student)
+    res.end(JSON.stringify(student))
+    res.end(student)
 
-//     res.statusCode = 200;
-//     res.setHeader("Content-Type", "text/plain");
-//     res.setHeader("X-App-Name", "My Node App");
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.setHeader("X-App-Name", "My Node App");
 
-//     res.end("Request received successfully");
-// })
+    res.end("Request received successfully");
+})
 
 
 // Routing Requests
-// const server=http.createServer((req,res)=>{
+const server5=http.createServer((req,res)=>{
 
-//     if(req.method==="GET" && req.url==="/"){
-//         res.end("Home page")
-//     }else if(req.method==="GET" && req.url==="/about"){
-//         res.end("About page")
-//     }else if(req.method==="GET" && req.url==="/contact"){
-//         res.end("contact page")
-//     }else if(req.method==="GET" && req.url==="/product"){
-//         res.end("Product page")
-//     }else{
-//         res.statusCode=404;
-//         res.end("Page Not Found")
-//     }
-// })
+    if(req.method==="GET" && req.url==="/"){
+        res.end("Home page")
+    }else if(req.method==="GET" && req.url==="/about"){
+        res.end("About page")
+    }else if(req.method==="GET" && req.url==="/contact"){
+        res.end("contact page")
+    }else if(req.method==="GET" && req.url==="/product"){
+        res.end("Product page")
+    }else{
+        res.statusCode=404;
+        res.end("Page Not Found")
+    }
+})
 
 
 // accessing query paramater using URL  
-// const server=http.createServer((req,res)=>{
+const server2=http.createServer((req,res)=>{
 
-//     const url = new URL(req.url, `http://${req.headers.host}`);
-//     console.log(req.headers.host)
-//     console.log(url.pathname);
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    console.log(req.headers.host)
+    console.log(url.pathname);
 
-//     const id = url.searchParams.get("id");
-//     console.log(url)
-//     console.log(id);
+    const id = url.searchParams.get("id");
+    console.log(url)
+    console.log(id);
 
-//     res.end("Done");
-// });
+    res.end("Done");
+});
 
-//Redirecting Requests
+// Redirecting Requests
 
-// const server=http.createServer((req,res)=>{
+const server3=http.createServer((req,res)=>{
 
-//     if(req.method==="GET" && req.url==="/old-page"){
+    if(req.method==="GET" && req.url==="/old-page"){
 
-//         res.statusCode=301;
-//         res.setHeader("Location","/new-page")
-//         res.end();
-//     }else if(req.url==="/new-page"){
-//         res.end("Welcome to new Page")
-//     }else{
-//         res.statusCode=404;
-//         res.end("Page Not Found")
-//     }
-// })
+        res.statusCode=301;
+        res.setHeader("Location","/new-page")
+        res.end();
+    }else if(req.url==="/new-page"){
+        res.end("Welcome to new Page")
+    }else{
+        res.statusCode=404;
+        res.end("Page Not Found")
+    }
+})
 
 //Parsing Request Bodies
 
-const server=http.createServer((req,res)=>{
+const server4=http.createServer((req,res)=>{
     let body="";
     req.on("data",(chunk)=>{
         body+=chunk;
